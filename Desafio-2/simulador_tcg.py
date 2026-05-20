9824b7bda83891496d24e950f1991aefec9c0d04
pedroksp <pedroksp@ic.ufrj.br>
2026-05-20 16:03:55 -0300

    feat: Criação das entradas de dados

---
+++ Desafio-2/simulador_tcg.py
@@ -0,0 +1,56 @@
+#Função que valida os valores inteiros digitados, se forem nulos/negativos, encerra o programa imediatamente
+def valida_num(valor):
+    if valor < 0:
+        raise ValueError("Valores negativos são inválidos! Tente novamente!")
+    
+    elif valor == 0:
+        raise ValueError("Valores nulos são inválidos! Tente novamente!")
+        
+    return valor
+
+#Loops de entrada para o Monstro 1 e 2
+while True:
+    try:
+        nome_m1 = str(input("Digite o nome do Monstro 1: "))
+
+        hp_m1 = valida_num(int(input("Digite os pontos de vida (HP) do Monstro 1: ")))
+
+        pts_ataque_m1 = valida_num(int(input("Digite os pontos de ataque do Monstro 1: ")))
+
+    except ValueError as e:
+        if "nulos" in str(e) or "negativos" in str(e):
+            print(f"\nErro no Monstro 1: {e}")
+
+        else:
+            print("\nAlguma entrada não foi aceita no Monstro 1.")
+
+        print("Operação encerrada.")
+        exit()
+
+while True:
+    try:
+        nome_m2 = str(input("Digite o nome do Monstro 2: "))
+
+        hp_m2 = valida_num(int(input("Digite os pontos de vida (HP) do Monstro 2: ")))
+
+        pts_ataque_m2 = valida_num(int(input("Digite os pontos de ataque do Monstro 2: ")))
+
+    except ValueError as e:
+        if "nulos" in str(e) or "negativos" in str(e):
+            print(f"\nErro no Monstro 2: {e}")
+
+        else:
+            print("\nAlguma entrada não foi aceita no Monstro 2.")
+
+        print("Operação encerrada.")
+        exit()
+
+
+
+
+        
+
+        
+        
+
+        
